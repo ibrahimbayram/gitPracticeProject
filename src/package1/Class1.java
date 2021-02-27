a0a0eba50d6264eb5899c4ddc0b8c90ca469d5e0
Ibrahim Bayram <ibrahim33bayram@gmail.com>
2021-02-27 21:30:22 +0100

    fift commit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -5,9 +5,9 @@ public class Class1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		int numberOfStudents = 71;
-		//Here is my changes!
 		double num =34.5;
-		
+		//Here is my changes!
+		//Today we have seen mew commands on GitHub
 	}
 
 }
